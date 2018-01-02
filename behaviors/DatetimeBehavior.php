bf1570989aba5bab2e2cefa476329c317575fa59
amuluowin <amuluowin@163.com>
2018-01-02 16:39:47 +0800

    add datetimebehavior

---
+++ behaviors/DatetimeBehavior.php
@@ -0,0 +1,13 @@
+<?php
+
+class DatetimeBehavior extends \yii\behaviors\TimestampBehavior
+{
+    protected function getValue($event)
+    {
+        if ($this->value === null) {
+            return date('Y-m-d H:i:s', time());
+        }
+
+        return parent::getValue($event);
+    }
+}
