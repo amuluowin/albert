0577b6db0bf0e051392e654d25fdb9be3e23d22a
albert <63851587@qq.com>
2018-10-12 01:28:15 +0800

    add timer to flush logs

---
+++ seaslog/LogBufferflush.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/12
+ * Time: 0:59
+ */
+
+namespace yii\swoole\seaslog;
+
+
+use yii\base\Component;
+use yii\swoole\base\BootInterface;
+use yii\swoole\server\Server;
+
+class LogBufferflush extends Component implements BootInterface
+{
+    /**
+     * @var int
+     */
+    public $ticket = 1;
+
+    public function handle(Server $server = null)
+    {
+        if (!$server->server->taskworker) {
+            //        \Swoole\Runtime::enableCoroutine();
+            \Swoole\Timer::tick($this->ticket * 1000, function (int $tick_id) {
+//                go(function () {
+                    \Seaslog::flushBuffer();
+//                });
+            });
+        }
+    }
+}
